3826ddbf1922b93a86ddbfe7534955c6a3ce2730
Yaz1018 <garoncdavis@gmail.com>
2020-04-05 22:57:02 -0400

    Hours tics are now styled appropriately and clock is fully functional

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,23 +1,26 @@
 import { LightningElement } from 'lwc';
-
+import { clockData } from './clockData';
 export default class App extends LightningElement {
     secs = new Date().getSeconds() * 6;
     mins = new Date().getMinutes() * 6;
     hours = new Date().getHours() * 30;
+    testStyle = "transform:rotate(60deg)"
     secondHand;
     minuteHand;
     hourHand;
+    hourTic;
+    clockHourData = [];
     seconds(d) {
         var n = d.getSeconds() * 6;
         this.secs = n;
         this.secondHand.style.transform = `rotate(${this.secs}deg)`
-        console.log(this.secondHand)  
+        //console.log(this.secondHand)  
     }
     minutes(d) {
         var n = d.getMinutes() * 6;
         this.mins = n;
         this.minuteHand.style.transform = `rotate(${this.mins}deg)`
-        console.log(this.minuteHand)  
+        //console.log(this.minuteHand)  
     }
     clockHours(d) {
         var n = d.getHours();
@@ -30,24 +33,45 @@ export default class App extends LightningElement {
             this.hours = n;
             this.hourHand.style.transform = `rotate(${this.hours}deg)`
         }
-        console.log(this.hourHand)  
+        //console.log(this.hourHand)  
     }
+
     myClock() {
         var d = new Date();
+
         this.seconds(d)
         this.minutes(d)
         this.clockHours(d)
+        //console.log(this.clockHourData)
+
 
     }
       
     myVar = setInterval(this.myClock.bind(this), 1000);
+    connectedCallback() {
+        console.log('clockHours', clockData)
+        //clockData.forEach(this.myFunction);
+
+        clockData.forEach(x => {
+            x.style = "transform:rotate(" + x.hour * 30 + "deg)"
+            this.clockHourData.push(x)
+        })
+        // .then(results => {
+        //     //this.clockHourData = results
+        //     console.log(results)
+        // })
+    }
     renderedCallback() {
-        console.log("In connected Callback")
+
+        //console.log("In connected Callback")
+        //console.log('clockHours', this.clockHourData)
         this.secondHand = this.template.querySelector(".second-hand");
         this.minuteHand = this.template.querySelector(".minute-hand");
         this.hourHand = this.template.querySelector(".hour-hand");
+        this.hourTic = this.template.querySelector(".hourTic");
 
-        console.log("renderedCallback", this.secondHand) 
+
+        //console.log("renderedCallback", this.secondHand) 
     }
 
 }
---
+++ src/client/modules/my/app/clockData.js
@@ -0,0 +1,50 @@
+export const clockData = [
+    { 
+        hour: 1,
+        style: ""
+    },
+    { 
+        hour: 2,
+        style: ""
+    },
+    { 
+        hour: 3,
+        style: ""
+    },
+    { 
+        hour: 4,
+        style: ""
+    },
+    { 
+        hour: 5,
+        style: ""
+    },
+    { 
+        hour: 6,
+        style: ""
+    },
+    { 
+        hour: 7,
+        style: ""
+    },
+    { 
+        hour: 8,
+        style: ""
+    },
+    { 
+        hour: 9,
+        style: ""
+    },
+    { 
+        hour: 10,
+        style: ""
+    },
+    { 
+        hour: 11,
+        style: ""
+    },
+    { 
+        hour: 12,
+        style: ""
+    },
+];
